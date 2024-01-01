20646a8666eee74b63fdf9ed74972fd2e69d0903
ngangavic <ngangavictor10@gmail.com>
2024-01-01 18:07:12 +0000

    subtract function

--- calculator.js
+++ calculator.js
@@ -5,4 +5,12 @@ function add(numbers) {
         .reduce((a, b) => a + b)
 }
 
+function subtract(numbers) {
+    return numbers
+        .split(',')
+        .map(x => parseInt(x))
+        .reduce((a, b) => a - b)
+}
+
 exports.add = add;
+exports.subtract = subtract;
