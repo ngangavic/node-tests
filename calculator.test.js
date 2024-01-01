17890fcc8dfb5bc3f6bd9356047c8a560d2afadb
ngangavic <ngangavictor10@gmail.com>
2024-01-01 18:07:24 +0000

    subtract test

--- calculator.test.js
+++ calculator.test.js
@@ -2,8 +2,8 @@ const calculator = require('./calculator')
 
 
 test('string with a single number should result in the number itself', () => {
-    //will pass
-    expect(calculator.add('1',1)).toBe(1);
     //will fail
-    expect(calculator.add('a',1)).toBe(1);
+    expect(calculator.subtract('b',1)).toBe(1);
+    //will pass
+    expect(calculator.subtract('1',1)).toBe(1);
   });
